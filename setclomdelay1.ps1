2c523a2e70faf987f6fc76b9f203b6e6e7edb1ca
Chris Nakagaki <zsoldier@gmail.com>
2018-02-01 10:31:19 -0500

    Update and rename setclomdelay1.txt to setclomdelay1.ps1
    Changed $hostpass variable to request 'credentials' for more flexibility of username and obfuscation of password entry.

--- setclomdelay1.ps1
+++ setclomdelay1.ps1
@@ -14,12 +14,12 @@ $vCenter = Read-Host "Provide vCenter Server"
 Connect-VIServer $vCenter -credential ( Get-Credential ) -WarningAction Silentlycontinue | Out-Null
 $cluster = Read-Host "Enter VSAN Cluster name"
 $clomdelay = Read-Host "Enter the delay time (in minutes)"
-Get-VMHost | Get-AdvancedSetting -name "VSAN.ClomRepairDelay" | Out-Null
+#Get-VMHost | Get-AdvancedSetting -name "VSAN.ClomRepairDelay" | Out-Null
 Get-AdvancedSetting -Entity (Get-VMHost) -Name "VSAN.ClomRepairDelay" | Set-AdvancedSetting -Value $clomdelay -Confirm:$false | Out-Null 
 Write-Host "Setting VSAN.ClomRepairDelay to $clomdelay minutes on each host"
 #
-$hostpass = Read-Host "Please enter root password for the ESXi hosts"
-$str1=’ECHO Y | c:\temp\plink.exe -pw $hostpass -l root ‘
+$hostpass = Get-Credential -Message "Enter root creds for ESXi hosts." 
+$str1="ECHO Y | c:\temp\plink.exe -pw $($hostpass.getnetworkcredential().password) -l $($hostpass.username) "
 $str2=’ /etc/init.d/clomd restart 2>&1’
 $outfile=’c:\temp\report.txt'
     foreach($esxentry in (Get-VMHost|?{$_.Powerstate -eq “PoweredOn”})){
